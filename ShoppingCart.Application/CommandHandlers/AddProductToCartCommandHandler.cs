64067476d866d5bdc8771944cf6e1b0e9f84b94a
Oliwier Matus <matusoliwier@gmail.com>
2025-06-11 22:58:29 +0200

    ShoppingCart init

---
+++ ShoppingCart.Application/CommandHandlers/AddProductToCartCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using EShop.Domain.Models;
+using MediatR;
+using ShoppingCart.Domain.Command;
+using ShoppingCart.Domain.Interfaces;
+
+namespace ShoppingCart.Application.CommandHandlers;
+
+public class AddProductToCartCommandHandler : IRequestHandler<AddProductToCartCommand>
+
+{
+    private readonly ICartAdder _cartAdder;
+
+    public AddProductToCartCommandHandler(ICartAdder cartAdder)
+    {
+        _cartAdder = cartAdder;
+    }
+
+    public Task Handle(AddProductToCartCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Id = command.ProductId
+        };
+        _cartAdder.AddProductToCart(command.CartId, product);
+        return Task.CompletedTask;
+    }
+}
---
+++ ShoppingCart.Application/CommandHandlers/RemoveProductFromCartCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using ShoppingCart.Domain.Command;
+using ShoppingCart.Domain.Interfaces;
+
+namespace ShoppingCart.Application.CommandHandlers;
+
+public class RemoveProductFromCartCommandHandler : IRequestHandler<RemoveProductFromCartCommand>
+{
+    private readonly ICartRemover _cartRemover;
+
+    public RemoveProductFromCartCommandHandler(ICartRemover cartRemover)
+    {
+        _cartRemover = cartRemover;
+    }
+
+    public Task Handle(RemoveProductFromCartCommand command, CancellationToken cancellationToken)
+    {
+        _cartRemover.RemoveProductFromCart(command.CartId, command.ProductId);
+        return Task.CompletedTask;
+    }
+}
